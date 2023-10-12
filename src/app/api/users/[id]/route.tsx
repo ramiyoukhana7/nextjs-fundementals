6b7893e6b01920408d6eb1641e8507b2630ba4f0
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-12 23:01:41 +0200

    Creating PUT and DELETE api

--- src/app/api/users/[id]/route.tsx
+++ src/app/api/users/[id]/route.tsx
@@ -12,3 +12,27 @@ export function GET(
     return NextResponse.json({ error: "user not found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: "Mosh" });
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: body.name });
+}
+
+export function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({});
+}
