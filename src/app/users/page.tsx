699e082dfeeb7ab16c75c8f7df689e0f6396d775
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-12 15:11:37 +0200

    testing with caching, static and dynamic rendering
    Server components are great for fetching data because they don’t require extra server trips, making our application faster and more search-engine friendly.
    
    Next.js enhances the fetch() function by adding automatic caching. This boosts performance and reduces the need to retrieve the same piece of data twice.
    
    In Next.js, components can be rendered at build time (called Static Rendering) or at request time (called Dynamic Rendering). If we have pages or components with static data, we can pre-render them during build time to improve our application’s performance.

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -6,12 +6,17 @@ interface User {
 }
 
 const UserPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/users",
+    //Hämtar data efter varje 10 sekunder
+    { cache: "no-store" }
+  );
   const users: User[] = await res.json();
 
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
