df4f1de3329ad74ac08967ada7b31e0bb854f26f
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-12 19:48:44 +0200

    Programmatic navigation

--- src/app/users/new/page.tsx
+++ src/app/users/new/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-const NestedUserPage = () => {
-  return <div>NestedUserPage</div>;
+const NewUserPage = () => {
+  const router = useRouter();
+
+  return (
+    <button className="btn btn-primary" onClick={() => router.push("/users")}>
+      Create
+    </button>
+  );
 };
 
-export default NestedUserPage;
+export default NewUserPage;
