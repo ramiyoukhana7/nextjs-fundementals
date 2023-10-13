dbe419f03b60d480e1a7bba52c90e7753ee0c400
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-13 13:55:30 +0200

    Storing products in prisma database

--- src/app/api/product/route.tsx
+++ src/app/api/product/route.tsx
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "Milk", price: 2.5 },
-    { id: 2, name: "Bread", price: 3.5 },
-  ]);
+import prisma from "../../../../prisma/client";
+
+export async function GET(request: NextRequest) {
+  const products = await prisma.product.findMany();
+  return NextResponse.json(products);
 }
 
 export async function POST(request: NextRequest) {
@@ -15,8 +15,12 @@ export async function POST(request: NextRequest) {
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  return NextResponse.json(
-    { id: 10, name: body.name, price: body.price },
-    { status: 201 }
-  );
+  const createProduct = await prisma.product.create({
+    data: {
+      name: body.name,
+      price: body.price,
+    },
+  });
+
+  return NextResponse.json(createProduct, { status: 201 });
 }
