c28c7e333650c8fab62d5405ab48691f87e6c26b
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-13 19:40:50 +0200

    Styling emails with Tailwind

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -3,6 +3,7 @@ import {
   Html,
   Body,
   Container,
+  Tailwind,
   Text,
   Link,
   Preview,
@@ -12,22 +13,16 @@ const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome aboard!</Preview>
-      <Body style={body}>
-        <Container>
-          <Text style={heading}>Hello {name}</Text>
-          <Link href="https://ramiwebsite.com">www.ramiwebsite.com</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="font-bold text-3xl">Hello {name}</Text>
+            <Link href="https://ramiwebsite.com">www.ramiwebsite.com</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
-};
-
-const body: CSSProperties = {
-  background: "#fff",
-};
-
-const heading: CSSProperties = {
-  fontSize: "32px",
 };
 
 export default WelcomeTemplate;
