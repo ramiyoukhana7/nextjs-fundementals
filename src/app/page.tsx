bf22cbe3d294c4f8ddc96be127bac71a78e13782
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-13 18:09:16 +0200

    Accessing session on the server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       {/*We navigate with Link instead of an Anchor tag to increase website performance.
       We only replace the content area
       and not redownloading the font, css and javaScript files*/}
