0f3d1de846cd5ab83b8479b2ef7f239fdfc77d9c
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-14 14:39:35 +0200

    Lazy Loading & Lodash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,30 @@ import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import { Metadata } from "next";
+import { useState } from "react";
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
+/*const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
+  ssr: false,
+  loading: () => <p>Loading ..</p>,
+}); */
+
+export default function Home() {
+  //Had to remove Async because client component can not be Async
+
+  const [isVisiable, setIsVisiable] = useState(false);
 
   return (
     <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+      <button
+        onClick={async () => {
+          const x = (await import("lodash")).default;
+          //const users = [{ name: "c" }, { name: "b" }, { name: "a" }];
+
+          //const sorted = _.orderBy(users, ["name"]);
+        }}
+      >
+        Show
+      </button>
       {/*We navigate with Link instead of an Anchor tag to increase website performance.
       We only replace the content area
       and not redownloading the font, css and javaScript files*/}
